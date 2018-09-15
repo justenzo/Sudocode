8865ebddc07a6f1b0fb7dab8ab013a9ba21c4a6d
szabaldo <sam.zabaldo@gmail.com>
2018-09-15 18:25:37 -0400

    finished
    all methods and unit tests

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -21,80 +21,12 @@ public class LatinSquare
 		return this.puzzle;
 	}
 	
+	
 	public void setLatinSquare(int[][] twoDimArr)
 	{
 		this.puzzle = twoDimArr;
 	}
 	
-	public int[] getColumn(int column)
-	{
-		int[] returnColumn = new int[this.puzzle.length];
-		
-		for (int i = 0; i < this.puzzle.length; i ++)
-		{
-			returnColumn[i] = this.puzzle[i][column];
-		}
-		return returnColumn;
-	}
-	
-	public int[] getRow(int row)
-	{
-		int[] returnRow = new int[this.puzzle.length];
-		
-		for (int i = 0; i < this.puzzle.length; i ++)
-		{
-			returnRow[i] = this.puzzle[row][i];
-		}
-		return returnRow;
-	}	
-	
-	public boolean containsZero() 
-	{
-		for(int iCol = 0; iCol < puzzle.length; iCol++)
-		{
-			for(int iRow = 0; iRow < puzzle.length; iRow++)
-			{
-				if (puzzle[iCol][iRow] == 0)
-				{
-					return true; 
-				}
-			}
-		}
-		return false; 
-	}
-	
-	public boolean doesElementExist(int[] arr, int iValue) 
-	{
-		for (int i : arr)
-		{
-			if (i == iValue)
-				return true; 
-		}
-		return false; 
-	}
-	
-	public boolean hasAllValues(int[] targetArr, int[] sourceArr)
-	{
-		boolean valueFound = false; 
-		
-		for (int s = 0; s < sourceArr.length; s ++)
-		{
-			valueFound = false; 
-			for (int t = 0; t < targetArr.length; t ++)
-			{
-				if (targetArr[t] == sourceArr[s])
-				{
-					valueFound = true;
-					t = targetArr.length - 1; 
-				}
-			}
-			if (valueFound == false)
-			{
-				return false; 
-			}
-		}
-		return true; 
-	}
 	
 	public boolean hasDuplicates(int[] oneDimArr) 
 	{
@@ -109,13 +41,67 @@ public class LatinSquare
 			}
 		}
 		return false; 
+	} 
+	
+	
+	public boolean doesElementExist(int[] arr, int iValue) 
+	{
+		for (int i = 0; i < arr.length; i ++)
+		{
+			if (arr[i] == iValue)
+			{
+				return true; 
+			}
+		}
+		return false; 
 	}
+	
+	
+	public boolean hasAllValues(int[] targetArr, int[] sourceArr)
+	{
+		for (int i = 0; i < sourceArr.length; i ++)
+		{
+			for (int j = 0; j < targetArr.length; j ++)
+			{
+				if (doesElementExist(getRow(i), targetArr[j]) == false)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	
+	public int[] getColumn(int column)
+	{
+		int[] returnColumn = new int[this.puzzle.length];
+		
+		for (int i = 0; i < this.puzzle.length; i ++)
+		{
+			returnColumn[i] = this.puzzle[i][column];
+		}
+		return returnColumn;
+	}
+	
+	
+	public int[] getRow(int row)
+	{
+		int[] returnRow = new int[this.puzzle.length];
+		
+		for (int i = 0; i < this.puzzle.length; i ++)
+		{
+			returnRow[i] = this.puzzle[row][i];
+		}
+		return returnRow;
+	}	
+	
 	
 	public boolean isLatinSquare() 
 	{
 		for (int i = 0; i < this.puzzle.length; i ++) 
 		{
-			if (hasDuplicates(getRow(i)) == true || hasDuplicates(getColumn(i)) == true) 
+			if (hasDuplicates(getRow(i)) == true || hasDuplicates(getColumn(i)) == true || containsZero() == true)
 			{
 				return false; 
 			}
@@ -129,5 +115,21 @@ public class LatinSquare
 			}
 		}
 		return true; 
+	}
+	
+	
+	public boolean containsZero() 
+	{
+		for(int iCol = 0; iCol < puzzle.length; iCol++)
+		{
+			for(int iRow = 0; iRow < puzzle.length; iRow++)
+			{
+				if (puzzle[iCol][iRow] == 0)
+				{
+					return true; 
+				}
+			}
+		}
+		return false; 
 	}
 }
--- SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
+++ SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
@@ -2,6 +2,8 @@ package pkgHelper;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class LatinSquareTest 
@@ -10,22 +12,80 @@ public class LatinSquareTest
 	@Test
 	public void test() 
 	{
-		int goodSquare[][] = { { 1, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2} };
-		int badSquare[][] = { { 1, 2, 2 }, { 2, 3, 1 }, { 3, 1, 2} };
+		int twoDimArray[][] = { { 1, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 } };
+		int twoDimArray2[][] = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+		int oneDimArray[] = { 1, 2, 3 };
 		
-		LatinSquare testGood = new LatinSquare(goodSquare);
-		LatinSquare testBad = new LatinSquare(badSquare);
+		LatinSquare testSquare = new LatinSquare(twoDimArray);
 		
 		
+		//JUnit Tests
+		
+		
+		//getLatinSquare test cases
+		assertTrue(Arrays.equals(testSquare.getLatinSquare(), twoDimArray));
+		assertFalse(Arrays.equals(testSquare.getLatinSquare(), twoDimArray2));
+		
+		
+		//setLatinSquare test cases
+		testSquare.setLatinSquare(twoDimArray2);
+		assertTrue(Arrays.equals(testSquare.getLatinSquare(), twoDimArray2));
+		testSquare.setLatinSquare(twoDimArray);
+		assertFalse(Arrays.equals(testSquare.getLatinSquare(), twoDimArray2));
+		
+		
+		//hasDuplicates test cases
+		assertFalse(testSquare.hasDuplicates(oneDimArray));
+		oneDimArray[0] = 2; //adjusting array to change the method's output
+		assertTrue(testSquare.hasDuplicates(oneDimArray));
+		oneDimArray[0] = 1; //reverting array to original
+		
+		
+		//doesElementExist test cases
+		assertTrue(testSquare.doesElementExist(oneDimArray, 1));
+		assertFalse(testSquare.doesElementExist(oneDimArray, 0));
+		
+		
+		//hasAllValues test cases
+		assertTrue(testSquare.hasAllValues(testSquare.getColumn(1),testSquare.getColumn(0)));
+		twoDimArray[0][0] = 5; //adjusting array to change the method's output
+		assertFalse(testSquare.hasAllValues(testSquare.getColumn(1),testSquare.getColumn(0)));
+		twoDimArray[0][0] = 1; //reverting array to original
+		
+		
+		//getColumn test cases
+		assertTrue(Arrays.equals(testSquare.getRow(0), oneDimArray));
+		oneDimArray[0] = 4; //adjusting array to change the method's output
+		assertFalse(Arrays.equals(testSquare.getRow(0), oneDimArray));
+		oneDimArray[0] = 1; //reverting array to original
+		
+		
+		//getRow test cases
+		assertTrue(Arrays.equals(testSquare.getRow(0), oneDimArray));
+		oneDimArray[0] = 4; //adjusting array to change the method's output
+		assertFalse(Arrays.equals(testSquare.getRow(0), oneDimArray));
+		oneDimArray[0] = 1; //reverting array to original
+		
+		
+		//isLatinSquare test cases
+		assertTrue(testSquare.isLatinSquare());
+		twoDimArray[0][0] = 2; //would be a latin square, but it has a duplicate
+		assertFalse(testSquare.isLatinSquare());
+		twoDimArray[0][0] = 4; //would be a latin square, but it doesnt have all values
+		assertFalse(testSquare.isLatinSquare());
+		twoDimArray[0][0] = 0; //would be a latin square, but has zeros
+		twoDimArray[1][2] = 0;
+		twoDimArray[2][1] = 0;
+		assertFalse(testSquare.isLatinSquare());
+		twoDimArray[0][0] = 1; //reverting array to original 
+		twoDimArray[1][2] = 1;
+		twoDimArray[2][1] = 1;
+		
+	
 		//containsZero test cases
-		assertFalse(testGood.containsZero());
-		goodSquare[0][1] = 0; 
-		assertTrue(testGood.containsZero());
-		goodSquare[0][1] = 2; 
-		
-		
-		//assertFalse(testBad.containsZero());
-		
-		
+		assertFalse(testSquare.containsZero());
+		twoDimArray[0][1] = 0; //adjusting array to change the method's output
+		assertTrue(testSquare.containsZero());
+		twoDimArray[0][1] = 2; //reverting array to original
 	}
 }
